7898e5f2c7e7ff892026b0ca050a054673318a55
Mehrdad Yazdani <myazdani@gmail.com>
2015-03-25 06:04:42 -0700

    fixed bug

--- scripts/resize_images.py
+++ scripts/resize_images.py
@@ -1,13 +1,13 @@
 import os
 from PIL import Image 
 
-file_save_type = ".bmp"
+file_save_type = ".gif"
 
-src_path, image_type = "path_to_parent_directory_of_images", ".jpg"
+src_path, image_type = "/Users/myazdaniUCSD/Documents/misc_scripts/norooz2048/img/212", ".jpg"
  
 image_paths = []  
 for root, dirs, files in os.walk(src_path):
-  image_paths.extend([os.path.join(root, f) for f in files])
+  image_paths.extend([os.path.join(root, f) for f in files if f.endswith(image_type)])
 
 for image_path in image_paths:
 	pil_im = Image.open(image_path)
